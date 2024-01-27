af92c3a9a272ea68e60a3ff62b2796cd2ac67792
juliangoes89 <juliangomezes89@gmail.com>
2024-01-27 13:59:09 -0500

    adicionar la logica del background scroll

---
+++ src/components/Background.js
@@ -0,0 +1,12 @@
+export default class Background extends Phaser.GameObjects.GameObject{
+    constructor(scene){
+        super(scene);
+        this.scene = scene;
+        this.gameWidth = this.scene.game.config.width;
+        this.gameHeight = this.scene.game.config.height;
+        this.background = this.scene.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, 'background').setOrigin(0, 0).setDepth(-1);
+    }
+    update(){
+        this.background.tilePositionY -= 1;
+    }
+}
